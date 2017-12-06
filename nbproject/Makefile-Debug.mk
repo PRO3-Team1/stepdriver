ffece88a59813eee83582d1d3b6a6222924ccc11
FuryFire <furyfire@users.noreply.github.com>
2017-12-06 13:43:33 +0100

    Removed GCC requirement

--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -67,7 +67,7 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/stepdriver: ${OBJECTFILES}
 ${OBJECTDIR}/main.o: main.cpp
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -Ivendor/EBBLibrary -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cpp
+	$(COMPILE.cc) -g -Ivendor/EBBLibrary -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cpp
 
 # Subprojects
 .build-subprojects:
