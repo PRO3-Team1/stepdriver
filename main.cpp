6192666eb7582be208a50ee6e430dbd96dca98d2
root <root@beaglebone.localdomain>
2018-02-01 14:47:13 +0100

    lidt ændring

--- main.cpp
+++ main.cpp
@@ -14,8 +14,6 @@
 #include <cstdlib>
 #include <iostream>
 #include <cstring>
-#include "unistd.h"
-#include "motor/StepperMotor.h"
 #include "gpio/PWM.h"
 
 using namespace std;
@@ -27,7 +25,7 @@ using namespace exploringBB;
 #define GPIOPIN_DRIVER1_MS3         65  //P8 - Pin 18
 #define GPIOPIN_DRIVER1_RESET       26  //P8 - Pin 14
 #define GPIOPIN_DRIVER1_SLEEP       46  //P8 - Pin 16
-#define GPIOPIN_DRIVER1_STEP        "bs_pwm_test_P9_14.15"
+#define GPIOPIN_DRIVER1_STEP        ""
 //#define GPIOPIN_DRIVER1_STEP        23  //P8 - Pin 13
 #define GPIOPIN_DRIVER1_DIRECTION   68  //P8 - Pin 10
 
@@ -37,23 +35,25 @@ using namespace exploringBB;
 #define GPIOPIN_DRIVER2_MS3         115 //P9 - Pin 27
 #define GPIOPIN_DRIVER2_RESET       112 //P9 - Pin 30
 #define GPIOPIN_DRIVER2_SLEEP       20  //P9 - Pin 41   Not exported?
-#define GPIOPIN_DRIVER2_STEP        50  //P9 - Pin 14
+#define GPIOPIN_DRIVER2_STEP        "bs_pwm_test_P9_14.15"  //P9 - Pin 14
 #define GPIOPIN_DRIVER2_DIRECTION   60  //P9 - Pin 12
 /*
  * 
  */
 int main(int argc, char** argv) {
-    PWM driver1_step(GPIOPIN_DRIVER1_STEP);
+    PWM driver2_step(GPIOPIN_DRIVER2_STEP);
     if(strcmp(argv[1],"start") == 0 && argc == 3)
     {
-        driver1_step.setFrequency(atof(argv[2]));
-        driver1_step.setDutyCycle(100000); //From A4988 datasheet - minimum high time
-        driver1_step.run();
+        cout << "Starting PWM with frequency of " << argv[2] << endl;
+        driver2_step.setFrequency(atof(argv[2]));
+        driver2_step.setDutyCycle((unsigned int)100000); //From A4988 datasheet - minimum high time
+        driver2_step.run();
     }
     
     if(argc == 2 && strcmp(argv[1],"stop") == 0)
     {
-        driver1_step.stop();
+        driver2_step.stop();
+        cout << "Stopping PWM" << endl;
     }
     
    return 0;
