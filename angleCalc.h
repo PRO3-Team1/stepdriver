1388fcbabdfb857cf21cddfb270bca7d4bcab044
Mikkel85 <sgt.mich@gmail.com>
2018-02-28 10:14:04 +0100

    angleCalc.h added

---
+++ angleCalc.h
@@ -0,0 +1,14 @@
+/*
+ * AngleCalc.h
+ *
+ *  Created on: 26. feb. 2018
+ *      Author: Mich
+ */
+
+#ifndef ANGLECALC_H_
+#define ANGLECALC_H_
+
+int angleCalc (double Ang);
+
+
+#endif /* ANGLECALC_H_ */
