220e55c0aef931a961c23cf001fa224f0a3bb224
furyfire <furyfire@users.noreply.github.com>
2018-05-08 14:59:44 +0200

    Update io.h

--- io.h
+++ io.h
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   io.h
- * Author: anders
+ * Author: TEAM1
  *
  * Created on March 5, 2018, 1:19 PM
  */
